1073064a99215c4c7d3ba91603bcbbee3b94d333
wx-chevalier <384924552@qq.com>
2022-02-05 19:06:30 +0800

    feat: update articles or snippets

--- dd-search-engine/sg-index-doc/src/cli/calc-sum.ts
+++ dd-search-engine/sg-index-doc/src/cli/calc-sum.ts
@@ -1,19 +1,21 @@
 import * as walkSync from "walk-sync";
 
 const destDir =
-  "/Users/zhangzixiong/Library/Containers/com.kingsoft.wpsoffice.mac/Data/Library/Application Support/Kingsoft/WPS Cloud Files/userdata/qing/filecache/27701737/联泰/财务、采购、公共关系/发票";
+  "/Users/zhangzixiong/Library/Containers/com.kingsoft.wpsoffice.mac/Data/Library/Application Support/Kingsoft/WPS Cloud Files/userdata/qing/filecache/27701737/联泰/财务、采购、公共关系/发票/2022/待用";
 
 const files = walkSync(destDir);
 
 console.log(
   files.reduce((prev, cur) => {
-    const fileName = cur.split("/")[2];
+    const fileName = cur.split("/")[1];
 
     if (!fileName || !fileName.endsWith("pdf")) {
       return prev;
     }
 
-    console.log(fileName, Number(fileName.split("-")[0]));
+    if (fileName.includes("(")) {
+      console.log(fileName, Number(fileName.split("-")[0]));
+    }
 
     return prev + (Number(fileName.split("-")[0]) || 0);
   }, 0)
