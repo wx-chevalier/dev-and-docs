a7ff0decf1468a6d0a6d8f259602bca5a52f148d
wx-chevalier <384924552@qq.com>
2022-02-11 12:07:56 +0800

    feat: update articles or snippets

--- dd-search-engine/sg-index-doc/src/cli/calc-sum.ts
+++ dd-search-engine/sg-index-doc/src/cli/calc-sum.ts
@@ -1,7 +1,7 @@
 import * as walkSync from "walk-sync";
 
 const destDir =
-  "/Users/zhangzixiong/Library/Containers/com.kingsoft.wpsoffice.mac/Data/Library/Application Support/Kingsoft/WPS Cloud Files/userdata/qing/filecache/27701737/联泰/财务、采购、公共关系/发票/2022/待报销/张梓雄/B1";
+  "/Users/zhangzixiong/Library/Containers/com.kingsoft.wpsoffice.mac/Data/Library/Application Support/Kingsoft/WPS Cloud Files/userdata/qing/filecache/27701737/联泰/财务、采购、公共关系/发票/2022/待报销/张梓雄/B1/消费";
 
 // const destDir =
 //   "/Users/zhangzixiong/Library/Containers/com.kingsoft.wpsoffice.mac/Data/Library/Application Support/Kingsoft/WPS Cloud Files/userdata/qing/filecache/27701737/联泰/财务、采购、公共关系/发票/2022/待分配";
@@ -9,7 +9,7 @@ const destDir =
 const files = walkSync(destDir);
 let count = 0;
 const totalNum = files.reduce((prev, cur) => {
-  const fileName = cur.split("/")[1];
+  const fileName = cur.split("/")[0];
 
   if (!fileName) {
     return prev;
